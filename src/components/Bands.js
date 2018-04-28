1526c56a0dad8b4775a0f6d939677fbf7bc65fdc
Ashley Muller <serenehopes@gmail.com>
2018-04-28 03:52:26 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -4,8 +4,8 @@ class Bands extends Component {
  
   render() {
       const bandsArray = this.props.bands
-   bands = bandsArray.map((band)=> {
-      <li><Band band={band}/></li>
+      bands = bandsArray.map((band)=> {
+      <li><Band band={band} key={band.id}/></li>
     })
   
     return(
