f8d10edf4b4990f5c9915317374bc79001167250
Ashley Muller <serenehopes@gmail.com>
2018-04-28 03:51:57 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 class Bands extends Component {
  
   render() {
+      const bandsArray = this.props.bands
+   bands = bandsArray.map((band)=> {
+      <li><Band band={band}/></li>
+    })
   
     return(
       <ul>
